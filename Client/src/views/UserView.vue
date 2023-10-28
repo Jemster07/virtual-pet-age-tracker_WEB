1e4aa1ca705980ba39a5d42495c97dfb1c6a842f
Jemster07 <emelie.kerek@gmail.com>
2023-10-27 23:37:41 -0400

    Edit pet form now auto populates with appropriate fields

--- Client/src/views/UserView.vue
+++ Client/src/views/UserView.vue
@@ -22,7 +22,7 @@
           <td>{{ pet.age }}</td>
           <td>
             <div class="buttons are-small">
-              <button class="button is-info" v-on:click="openModal()">Edit</button>
+              <button class="button is-info" v-on:click="editClicked(pet)">Edit</button>
               <button class="button is-info">R.I.P</button>
               <button class="button is-danger is-light">Delete</button>
             </div>
@@ -31,39 +31,57 @@
       </tbody>
     </table>
 
-    <div id="edit-modal" class="modal">
+    <div id="edit-modal" class="modal is-clipped">
       <div class="modal-background"></div>
       <div class="modal-content">
-        <div class="box">
-          
+        <div class="box">       
+          <label class="label is-size-4">Edit {{ activePet.petName }}</label>
           <div class="field">
-            <label class="label">Label</label>
+            <label class="label">Name</label>
               <div class="control">
-                <input class="input" type="text" placeholder="Text input">
-              </div>
-              <p class="help">This is a help text</p>
+                <input class="input" type="text" v-model="editPet.petName">
+              </div> 
+              <p class="help">Current Value: {{ activePet.petName }}</p>    
           </div>
-
           <div class="field">
-            <label class="label">Label</label>
+            <label class="label">Brand/Species</label>
               <div class="control">
-                <input class="input" type="text" placeholder="Text input">
+                <input class="input" type="text" v-model="editPet.brand">
               </div>
-              <p class="help">This is a help text</p>
+              <p class="help">Current Value: {{ activePet.brand }}</p>    
           </div>
-
+          <div class="field">
+            <label class="label">Type</label>
+              <div class="control">
+                <input class="input" type="text" v-model="editPet.petType">
+              </div>
+              <p class="help">Current Value: {{ activePet.petType }}</p>    
+          </div>
+          <div class="field">
+            <label class="label">Day of Birth</label>
+              <div class="control">
+                <input class="input" type="text" v-model="editPet.dateBirth">
+              </div>
+              <p class="help">Current Value: {{ activePet.dateBirth }}</p>    
+          </div>
+          <div class="field">
+            <label class="label">Time of Birth</label>
+              <div class="control">
+                <input class="input" type="text" v-model="editPet.timeBirth">
+              </div>
+              <p class="help">Current Value: {{ activePet.timeBirth }}</p>    
+          </div>
           <div class="field is-grouped is-grouped-right">
             <p class="control">
               <a class="button is-info">Submit</a>
             </p>
             <p class="control">
-              <a class="button is-light">Cancel</a>
+              <a class="button is-light" v-on:click="cancelForm()">Cancel</a>
             </p>
           </div>
-
         </div>
       </div>
-      <button class="modal-close is-large" aria-label="close" v-on:click="closeModal()"></button>
+      <button class="modal-close is-large" aria-label="close" v-on:click="cancelForm()"></button>
     </div>
   </div>
 </template>
@@ -90,9 +108,20 @@ export default {
         petType: "",
         brand: "",
         dateBirth: "",
-        timeBirth: null,
-        dateDeath: null,
-        isHidden: true,
+        timeBirth: "",
+        dateDeath: "",
+        isHidden: false,
+        userId: this.$store.state.user.userId,
+      },
+      activePet: {
+        petId: 0,
+        petName: "",
+        petType: "",
+        brand: "",
+        dateBirth: "",
+        timeBirth: "",
+        dateDeath: "",
+        isHidden: false,
         userId: this.$store.state.user.userId,
       },
     };
@@ -122,6 +151,35 @@ export default {
       const editModal = document.querySelector('#edit-modal');
       const list = editModal.classList;
       list.remove("is-active");
+    },
+    editClicked(pet) {
+      this.mapActivePet(pet);
+      this.mapEditPet(pet);
+      this.openModal();
+    },
+    cancelForm() {
+      this.activePet = {};
+      this.editPet = {};
+      this.newPet = {};
+      this.closeModal();
+    },
+    mapActivePet(pet) {
+      this.activePet.petId = pet.petId;
+      this.activePet.petName = pet.petName;
+      this.activePet.petType = pet.petType;
+      this.activePet.brand = pet.brand;
+      this.activePet.dateBirth = pet.dateBirth;
+      this.activePet.timeBirth = pet.timeBirth;
+      this.activePet.dateDeath = pet.dateDeath;
+    },
+    mapEditPet(pet) {
+      this.editPet.petId = pet.petId;
+      this.editPet.petName = pet.petName;
+      this.editPet.petType = pet.petType;
+      this.editPet.brand = pet.brand;
+      this.editPet.dateBirth = pet.dateBirth;
+      this.editPet.timeBirth = pet.timeBirth;
+      this.editPet.dateDeath = pet.dateDeath;
     },
   },
 };
